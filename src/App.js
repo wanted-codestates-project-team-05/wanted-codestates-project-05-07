0484706f260c39475d1891447937741cc83ac150
seungyeon.lee <dus3214@gmail.com>
2022-02-22 21:59:50 +0900

    create dataList nav

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import DataList from './components/dataList/DataList';
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <DataList/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
--- src/components/dataList/dataList.js
+++ src/components/dataList/dataList.js
@@ -1,7 +1,36 @@
 import React from 'react'
+import styled from 'styled-components';
+import { DataNav } from './DataNav';
 
-export default function dataList() {
+const DataList = () =>  {
   return (
-    <div>dataList</div>
-  )
+    <DataListContainer>
+      <DataNav />
+      <DataItemList>
+        
+      </DataItemList>
+    </DataListContainer>
+  );
 }
+
+const DataListContainer = styled.div`
+  background-color: #E2C4C9;
+  height: 100vh;
+`;
+
+const DataItemList = styled.article`
+  width: 90%;
+  height: calc(100vh - 300px);
+  background-color: #fff;
+  margin: 5em auto 0;
+  border-radius: 20px;
+  box-shadow: rgba(0, 0, 0, 0.07) 1.95px 1.95px 2.6px;
+  .none-list {
+    padding-top: 10em;
+    text-align: center;
+    color: gray;
+    box-sizing: border-box;
+  }
+`;
+
+export default DataList;
