b0d171b611cfe7d04c5f2f8b10caf5dbf308143b
Lee-Seonghyun316 <mn031621019@gmail.com>
2022-02-22 22:35:26 +0900

    Feat: 주소 입력창 클릭하면 주소찾기 모달창 open, 주소삽입 기능 추가
    주소가 선택되면 모달창이 꺼지면서 주소 입력창에 자동으로 주소 입력
    모달의 접근성을 고려한 포커스 이동
    theme 변수 사용을 위한 설정

--- src/components/createdForm/createdForm.js
+++ src/components/createdForm/createdForm.js
@@ -1,5 +1,74 @@
-import React from "react";
+import React, { useCallback, useRef, useState } from "react";
+import styled from "styled-components";
+import AddressModal from "./AddressModal";
 
-export default function createdForm() {
-  return <div>createdForm</div>;
-}
+const CreatedForm = () => {
+  const [isAddress, setIsAddress] = useState("");
+  const getAddress = (address) => {
+    setIsAddress(address);
+  };
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const modalRef = useRef(null);
+  const openButtonRef = useRef(null);
+  const handleClickClose = useCallback(() => {
+    setIsModalOpen(true);
+    setTimeout(() => modalRef.current.focus());
+  }, []);
+  const handleClickOpen = useCallback(() => {
+    setIsModalOpen(false);
+    // 모달의 웹 접근성을 위해 시간차를 두고 포커스 이동
+    setTimeout(() => openButtonRef.current.focus());
+  }, []);
+
+  return (
+    <Wrap>
+      <Fieldset>
+        <Label>배송지</Label>
+        <div
+          ref={openButtonRef}
+          onClick={handleClickClose}
+          aria-haspopup="true"
+          aria-pressed={isModalOpen}
+        >
+          <Input
+            type="text"
+            autocorrect="off"
+            autocapitalize="none"
+            autocomplete="off"
+            value={isAddress}
+          />
+        </div>
+      </Fieldset>
+      <AddressModal
+        isModalOpen={isModalOpen}
+        onModalClose={handleClickOpen}
+        modalRef={modalRef}
+        getAddress={getAddress}
+      />
+    </Wrap>
+  );
+};
+
+export default CreatedForm;
+
+const Wrap = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+`;
+
+const Fieldset = styled.fieldset``;
+
+const Label = styled.label`
+  font-weight: 600;
+  font-size: 0.75rem;
+`;
+
+const Input = styled.input`
+  width: 400px;
+  border: 1px solid black;
+  border-radius: 10px;
+  padding: 10px;
+  margin: 10px 0;
+`;
