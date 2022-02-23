59391cbb965ad407e54dfbe8840fb70595876395
Lee-Seonghyun316 <mn031621019@gmail.com>
2022-02-23 15:53:00 +0900

    Refactor: 주소 찾기 기능 및 제출버튼 전체 폼 연결

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from "react";
 import "./App.css";
 import "./reset.css";
-import CreatedForm from "./components/createdForm/CreatedForm";
+import AddressInput from "./components/createdForm/AddressInput";
 import { ThemeProvider } from "styled-components";
 import theme from "./theme";
+import SubForm from "./components/createdForm/SubForm";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      {/*<SubForm />*/}
-      <CreatedForm />
+      <SubForm />
+      {/*<AddressInput />*/}
     </ThemeProvider>
   );
 }
--- src/components/createdForm/SubForm.jsx
+++ src/components/createdForm/SubForm.jsx
@@ -1,19 +1,10 @@
-/* 
-1.폼의 양식은 서버에서 내려주며 포맷은 다음과 같습니다.
-3.휴대폰 번호(type = phone)의 경우 올바른 데이터 타입인지 확인합니다.
-
-
-2.모든 필수 폼(required = true)이 올바르게 입력되었을 때 “제출하기” 버튼이 활성화 됩니다.
-(name, phone, address, input_0(옵션), agreement_0(개인정보 수집 및 약관 내용))
-4.파일 첨부 시, 얼마나 업로드 되었는지 프로그레스바를 출력합니다.
-*/
-
-// 2. 옵션, 3. 개인정보 수집 약관 동의 (필수)
-
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import FormList from "./FormList";
 import { InputBox, SelectBox, AgreementBox } from "./hooks";
+import AddressInput from "./AddressInput";
+import SubmitButton from "./SubmitButton";
+import { submitForm } from "./submitForm";
 
 const Wrapper = styled.div`
   margin: 0 auto;
@@ -66,10 +57,14 @@ const AlertMessage = styled.span`
 `;
 
 function SubForm() {
+  //주소 변수 : sh
+  const [address, setAddress] = useState("");
+  // sh
+
   const [user, setUser] = useState({
     name: "",
     phone: "",
-    address: "",
+    address: address,
     select: "",
     option: "",
     file: "",
@@ -80,7 +75,6 @@ function SubForm() {
   // 유효성 검사
   const [isName, setIsName] = useState(false);
   const [isPhone, setIsPhone] = useState(false);
-  const [address, setAddress] = useState(false);
   const [option, setOption] = useState(false);
   const [agreement, setAgreement] = useState(false);
 
@@ -156,6 +150,25 @@ function SubForm() {
     }
   };
 
+  //제출하기 버튼 : sh
+  const [isSubmit, setIsSubmit] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [disabledSubmit, setDisabledSubmit] = useState(false);
+
+  const handleLoading = (loading) => {
+    setIsLoading(loading);
+  };
+
+  const handleClickSubmit = () => {
+    submitForm("김코딩", "010-4444-4444", "", "s", "", true, handleLoading)
+      .then((result) => {
+        console.log("제출 성공: ", result);
+        setIsSubmit(true);
+      })
+      .catch((error) => console.log("제출 실패: ", error));
+  };
+  //sh 끝
+
   return (
     <Wrapper>
       <Title>데이터블 폼 예시</Title>
@@ -192,13 +205,14 @@ function SubForm() {
               </>
             )}
             {form.id === "address" && (
-              <InputBox
+              <AddressInput
                 label={form.label}
                 nameMessage={nameMessage}
                 id={form.id}
                 type={form.type}
                 required={form.required}
-                value={user[form.id]}
+                value={address}
+                setValue={setAddress}
                 onChange={nameHandler}
               />
             )}
@@ -236,7 +250,12 @@ function SubForm() {
           </InputList>
         ))}
         <Footer>
-          <Submit type="submit" value="Submit" onClick={formSubmit}></Submit>
+          <SubmitButton
+            onClickSubmit={handleClickSubmit}
+            disabledSubmit={disabledSubmit}
+            isLoading={isLoading}
+            isSubmit={isSubmit}
+          />
         </Footer>
       </Form>
     </Wrapper>
