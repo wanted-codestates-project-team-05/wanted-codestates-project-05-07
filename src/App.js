6466c3cce0309eb55f50b571060f5db06304a90b
Lee-Seonghyun316 <mn031621019@gmail.com>
2022-02-23 14:34:05 +0900

    Chore: 랜더링 컴포넌트 변경

--- src/App.js
+++ src/App.js
@@ -4,13 +4,12 @@ import "./reset.css";
 import CreatedForm from "./components/createdForm/CreatedForm";
 import { ThemeProvider } from "styled-components";
 import theme from "./theme";
-import SubForm from "./components/createdForm/SubForm";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <SubForm />
-      {/*<CreatedForm />*/}
+      {/*<SubForm />*/}
+      <CreatedForm />
     </ThemeProvider>
   );
 }
